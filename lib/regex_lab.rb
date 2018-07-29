9243ed6090c221bd89a0c5c6673156d03711a872
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-29 02:18:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,14 +11,18 @@ end
 
 def words_five_letters_long(text)
   text.match(/\b\w{5}\b/)
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  numbers.match(/\A[A-Z].+[.,?!]\z/)
+  if numbers.match(/\A[A-Z].+[.,?!]\z/)
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d/).length == 10
-
+  if phone.scan(/\d/).length == 10
+else
+    false
+  end
 end
