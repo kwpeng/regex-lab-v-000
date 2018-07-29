b2fb4ae4a4bc6b94255cdbf446427ba31cf27750
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-29 02:17:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiouAEIOU]/)
+ if word.match(/\A[aeiouAEIOU]/)
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
