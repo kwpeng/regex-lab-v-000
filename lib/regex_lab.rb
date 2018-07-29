d455b3f3fd5635ed0a0d75114b6a081b262540f1
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-29 01:13:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/aeiou/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\wun[a-zA-Z])+ing/)
 end
 
 def words_five_letters_long(text)
+  text.grep(/^\w{5}\s/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  numbers.scan(/^[A-Z]+/)
 end
 
 def valid_phone_number?(phone)
+  phone.scan(/(\d{3})-(\d+)-(\d+)/)
 
 end
